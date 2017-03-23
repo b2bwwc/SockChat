61a667d084fccc1ac5758351aa122b51c9804fb3
Jeffrey Penkar <jspenkar@gmail.com>
2017-03-22 20:45:56 -0500

    App.js added.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+
+import App from './js/App'
+
+render(
+  <App />,
+  document.getElementById('app')
+)
